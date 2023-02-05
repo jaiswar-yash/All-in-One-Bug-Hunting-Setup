e907f768ca232772fbc0dc807518356455db7493
Yash Jaiswar <69403189+jaiswar-yash@users.noreply.github.com>
2023-02-05 09:01:20 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ echo "${G}[+] Installing go, if already go is installed then updating it${NC}"
 					echo 'export GOROOT=/usr/local/go' >> /home/$USER/.zshrc
 					echo 'export GOPATH=$HOME/go'	>> /home/$USER/.zshrc			
 					echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> /home/$USER/.zshrc
-
+					source /home/$USER/.zshrc
 #Directory to store Weapons 
 echo "${G}[+] Making a directory to Store your weapons${NC}"
 mkdir ~/Weapons
